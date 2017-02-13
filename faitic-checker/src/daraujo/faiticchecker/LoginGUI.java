a6eb6d9126913edbe8adac7505c90fc10f7bc89a
davovoid <david_04_13@hotmail.com>
2017-02-13 22:50:34 +0100

    Updating system, locales, about and login menu

--- faitic-checker/src/daraujo/faiticchecker/LoginGUI.java
+++ faitic-checker/src/daraujo/faiticchecker/LoginGUI.java
@@ -69,6 +69,7 @@ import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Semaphore;
 import java.awt.Cursor;
@@ -105,6 +106,8 @@ public class LoginGUI {
 	//private final static Image imgFaitic=new ImageIcon(LoginGUI.class.getResource("/daraujo/faiticchecker/logoFaitic.png")).getImage();
 	
 	private final static Image imgFaicheck=new ImageIcon(LoginGUI.class.getResource("/daraujo/faiticchecker/logoFaicheck.png")).getImage();
+
+	private final static Image iconSettings=new ImageIcon(LoginGUI.class.getResource("/daraujo/faiticchecker/iconSettings.png")).getImage();
 	
 	private final static Image spanishFlag=new ImageIcon(LoginGUI.class.getResource("/daraujo/faiticchecker/spain.png")).getImage();
 	private final static Image galicianFlag=new ImageIcon(LoginGUI.class.getResource("/daraujo/faiticchecker/galicia.png")).getImage();
@@ -128,7 +131,6 @@ public class LoginGUI {
 			fcnLogin();
 		}
 	};
-	private JPanel panel_1;
 	private JLabel lblConfigurationFolder;
 	private static JPanel panelUpdater;
 	private static JLabel lblUpdateHeader;
@@ -137,6 +139,11 @@ public class LoginGUI {
 	private static JLabel lblUpdate;
 	
 	private static Semaphore semLang=new Semaphore(1);
+	private JPanel panelSettings;
+	private JPanel panel_3;
+	private JCheckBox cCheckForUpdates;
+	private JPanel panel_1;
+	private JPanel panel_4;
 	
 	protected static String getJarPath(){
 
@@ -217,8 +224,8 @@ public class LoginGUI {
 					
 					if(updater.isThereANewVersion() && isTheJarPathAFile()){
 						
-						lblUpdateHeader.setText("Faicheck v." + updater.currentversion + " is available (You have v." + About.VERSION + ")");
-						txtUpdate.setText(updater.description + "\n\nMore info: " + updater.urlmoreinfo);
+						lblUpdateHeader.setText(textdata.getKey("updateheader", updater.currentversion, About.VERSION));
+						txtUpdate.setText(textdata.getKey("textupdate", updater.description, updater.urlmoreinfo));
 						panelUpdater.setVisible(true);
 						
 					} else{
@@ -559,7 +566,10 @@ public class LoginGUI {
 				
 				// Faicheck updater. Should be done as the last thing
 				System.out.println("My JAR file: " + getJarPath());
-				if(isTheJarPathAFile()) showUpdates();
+				
+				cCheckForUpdates.setSelected(!settings.jsonConf.containsKey("noupdatecheck"));
+				
+				if(isTheJarPathAFile() && !settings.jsonConf.containsKey("noupdatecheck")) showUpdates();
 				
 			}
 		});
@@ -594,8 +604,9 @@ public class LoginGUI {
 				int imgheight=imgwidth*imgFaicheck.getHeight(null)/imgFaicheck.getWidth(null);
 				
 				int loginposy=panelLogin != null ? panelLogin.getY() : 300;
+				int settingsendy=panelSettings!=null ? panelSettings.isVisible() ? panelSettings.getHeight() : 0 : 0;
 				
-				g2.drawImage(imgFaicheck, (super.getWidth()-imgwidth)/2, (loginposy-imgheight)/2, imgwidth, imgheight, null);
+				g2.drawImage(imgFaicheck, (super.getWidth()-imgwidth)/2, (loginposy-imgheight-settingsendy)/2 + settingsendy, imgwidth, imgheight, null);
 				
 			}
 			
@@ -608,6 +619,7 @@ public class LoginGUI {
 				FormFactory.MIN_COLSPEC,
 				FormFactory.GLUE_COLSPEC,},
 			new RowSpec[] {
+				FormFactory.PREF_ROWSPEC,
 				RowSpec.decode("default:grow(5)"),
 				FormFactory.MIN_ROWSPEC,
 				FormFactory.PARAGRAPH_GAP_ROWSPEC,
@@ -620,20 +632,87 @@ public class LoginGUI {
 				FormFactory.PREF_ROWSPEC,
 				FormFactory.UNRELATED_GAP_ROWSPEC,}));
 		
-		panel_1 = new JPanel();
-		panel_1.setOpaque(false);
-		panelWithEverything.add(panel_1, "3, 1, fill, fill");
-		panel_1.setLayout(new FormLayout(new ColumnSpec[] {
+		panelSettings = new JPanel(){
+			
+			@Override
+			public void paintComponent(Graphics g){
+				
+				g.setColor(new Color(240,240,240,255));
+				g.fillRect(0, 0, super.getWidth(), super.getHeight());
+				
+				g.setColor(new Color(70,70,70,255));
+				g.drawLine(0, super.getHeight()-1, super.getWidth(), super.getHeight()-1);
+				
+				for(int i=0; i<=5; i++){
+					g.setColor(new Color(70,70,70,90*(5-i)/5));
+					g.drawLine(0, super.getHeight()-2-i, super.getWidth(), super.getHeight()-2-i);
+				}
+				
+				Random random=new Random(1);
+				
+				for(int i=0; i<super.getHeight(); i++){
+					
+					g.setColor(new Color(255,255,255,50+random.nextInt(110)));
+					g.drawLine(0, i, super.getWidth(), i);
+					
+				}
+				
+			}
+			
+		};
+		panelSettings.setVisible(false);
+		panelSettings.setOpaque(false);
+		panelWithEverything.add(panelSettings, "1, 1, 3, 1, fill, fill");
+		panelSettings.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("12dlu"),
 				FormFactory.GLUE_COLSPEC,
+				FormFactory.UNRELATED_GAP_COLSPEC,
 				FormFactory.PREF_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,},
+				ColumnSpec.decode("12dlu"),},
 			new RowSpec[] {
-				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.PARAGRAPH_GAP_ROWSPEC,
+				RowSpec.decode("pref:grow"),
+				FormFactory.PARAGRAPH_GAP_ROWSPEC,}));
+		
+		
+		panel_3 = new JPanel();
+		panel_3.setOpaque(false);
+		panelSettings.add(panel_3, "2, 2, fill, fill");
+		panel_3.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.PREF_COLSPEC,
+				FormFactory.GLUE_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.GLUE_ROWSPEC,
 				FormFactory.PREF_ROWSPEC,
 				FormFactory.GLUE_ROWSPEC,}));
 		
+		cCheckForUpdates = new JCheckBox(textdata.getKey("checkforupdatesatstartup"));
+		cCheckForUpdates.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				
+				if(arg0.getStateChange()==arg0.SELECTED){
+					
+					// Check for updates automatically on start-up
+					
+					if(settings.jsonConf.containsKey("noupdatecheck")) settings.jsonConf.remove("noupdatecheck");
+					
+				} else{
+					
+					// Don't check for updates automatically
+					
+					if(!settings.jsonConf.containsKey("noupdatecheck")) settings.jsonConf.put("noupdatecheck","");
+					
+				}
+				
+				System.out.println("Check for updates: " + (arg0.getStateChange()==arg0.SELECTED ? "ON" : "OFF"));
+				
+			}
+		});
+		panel_3.add(cCheckForUpdates, "2, 2");
+		cCheckForUpdates.setOpaque(false);
+		
 		JPanel panel = new JPanel();
-		panel_1.add(panel, "2, 2");
+		panelSettings.add(panel, "4, 2");
 		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
 		flowLayout.setAlignment(FlowLayout.RIGHT);
 		panel.setOpaque(false);
@@ -748,6 +827,49 @@ public class LoginGUI {
 		pEnglish.setOpaque(false);
 		panel.add(pEnglish);
 		
+		panel_1 = new JPanel();
+		panel_1.setOpaque(false);
+		panelWithEverything.add(panel_1, "3, 2, fill, fill");
+		panel_1.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.GLUE_COLSPEC,
+				FormFactory.PREF_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.PREF_ROWSPEC,
+				FormFactory.GLUE_ROWSPEC,}));
+		
+		panel_4 = new JPanel(){
+			
+			@Override
+			public void paintComponent(Graphics g){
+				
+				super.paintComponent(g);
+
+				Graphics2D g2 = (Graphics2D) g;
+
+				g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, super.isEnabled() ? 1f : 0.3f));
+			    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			    g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+			    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+				g2.drawImage(iconSettings, 0, 0, super.getWidth(), super.getHeight(), null);
+				
+			}
+			
+		};
+		panel_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panelSettings.setVisible(!panelSettings.isVisible());
+			}
+		});
+		panel_4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		panel_4.setPreferredSize(new Dimension(30, 30));
+		panel_4.setMinimumSize(new Dimension(30, 30));
+		panel_4.setOpaque(false);
+		panel_1.add(panel_4, "2, 2, fill, fill");
+		
 		
 		panelLogin = new JPanel(){
 
@@ -779,7 +901,7 @@ public class LoginGUI {
 		};
 		panelLogin.setOpaque(false);
 		panelLogin.setMinimumSize(new Dimension(460, 280));
-		panelWithEverything.add(panelLogin, "2, 2, fill, fill");
+		panelWithEverything.add(panelLogin, "2, 3, fill, fill");
 		panelLogin.setLayout(new FormLayout(new ColumnSpec[] {
 				ColumnSpec.decode("48px"),
 				FormFactory.MIN_COLSPEC,
@@ -846,6 +968,41 @@ public class LoginGUI {
 		});
 		panel_2.add(btnLogin);
 		
+		panelUpdater = new JPanel(){
+
+			@Override
+			public void paintComponent(Graphics g){
+
+				//super.paintComponent(g);
+				
+				Color borderColor=super.getBackground();
+				
+
+				for(int i=0; i<8; i++){
+					
+					g.setColor(new Color(200,200,200,200*(i+1)/8));
+					
+					if(i%3==0){
+						g.drawLine(i/3, 7+i/3, super.getWidth()-i, 7+i/3);	// top
+						g.drawLine(i/3, 7+i/3+1, i/3, super.getHeight()-i-1);	// left
+					}
+
+					g.drawLine(super.getWidth()-i, 7+i/3+1, super.getWidth()-i, super.getHeight()-i-1);	// Right
+					g.drawLine(i/3, super.getHeight()-i, super.getWidth()-i, super.getHeight()-i);	// Bottom
+					
+				}
+
+				g.setColor(borderColor);
+				g.drawRect(3, 10, super.getWidth()-11, super.getHeight()-11-7);
+				g.drawRect(3, 10, super.getWidth()-12, super.getHeight()-12-7);
+				
+				
+			}
+
+		};
+		panelUpdater.setBackground(Color.LIGHT_GRAY);
+		panelUpdater.setVisible(false);
+		
 		panelStatus = new JPanel(){
 
 			@Override
@@ -881,50 +1038,15 @@ public class LoginGUI {
 		panelStatus.setVisible(false);
 		//panelStatus.setBackground(Color.RED);
 		panelStatus.setOpaque(false);
-		panelWithEverything.add(panelStatus, "2, 4, fill, fill");
+		panelWithEverything.add(panelStatus, "2, 5, fill, fill");
 		panelStatus.setLayout(new BorderLayout(0, 0));
 		
 		lblLoginStatus = new JLabel("Error text");
 		panelStatus.add(lblLoginStatus, BorderLayout.CENTER);
 		lblLoginStatus.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLoginStatus.setMinimumSize(new Dimension(85, 45));
-		
-		panelUpdater = new JPanel(){
-
-			@Override
-			public void paintComponent(Graphics g){
-
-				//super.paintComponent(g);
-				
-				Color borderColor=super.getBackground();
-				
-
-				for(int i=0; i<8; i++){
-					
-					g.setColor(new Color(200,200,200,200*(i+1)/8));
-					
-					if(i%3==0){
-						g.drawLine(i/3, 7+i/3, super.getWidth()-i, 7+i/3);	// top
-						g.drawLine(i/3, 7+i/3+1, i/3, super.getHeight()-i-1);	// left
-					}
-
-					g.drawLine(super.getWidth()-i, 7+i/3+1, super.getWidth()-i, super.getHeight()-i-1);	// Right
-					g.drawLine(i/3, super.getHeight()-i, super.getWidth()-i, super.getHeight()-i);	// Bottom
-					
-				}
-
-				g.setColor(borderColor);
-				g.drawRect(3, 10, super.getWidth()-11, super.getHeight()-11-7);
-				g.drawRect(3, 10, super.getWidth()-12, super.getHeight()-12-7);
-				
-				
-			}
-
-		};
-		panelUpdater.setBackground(Color.LIGHT_GRAY);
-		panelUpdater.setVisible(false);
 		panelUpdater.setOpaque(false);
-		panelWithEverything.add(panelUpdater, "2, 6, fill, fill");
+		panelWithEverything.add(panelUpdater, "2, 7, fill, fill");
 		panelUpdater.setLayout(new FormLayout(new ColumnSpec[] {
 				FormFactory.UNRELATED_GAP_COLSPEC,
 				FormFactory.GLUE_COLSPEC,
@@ -950,12 +1072,12 @@ public class LoginGUI {
 		txtUpdate.setVisible(false);
 		panelUpdater.add(txtUpdate, "2, 4, 4, 1, fill, fill");
 		
-		lblMoreInfo = new JLabel("More info");
+		lblMoreInfo = new JLabel(textdata.getKey("lblupdatemoreinfo"));
 		lblMoreInfo.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				
-				((JLabel)arg0.getComponent()).setText(txtUpdate.isVisible() ? "More info" : "Less info");
+				((JLabel)arg0.getComponent()).setText(txtUpdate.isVisible() ? textdata.getKey("lblupdatemoreinfo") : textdata.getKey("lblupdatelessinfo"));
 				panelLogin.setVisible(txtUpdate.isVisible());
 				txtUpdate.setVisible(!txtUpdate.isVisible());
 				
@@ -965,7 +1087,7 @@ public class LoginGUI {
 		lblMoreInfo.setForeground(new Color(0,110,198,255));
 		panelUpdater.add(lblMoreInfo, "3, 6");
 		
-		lblUpdate = new JLabel("Update now");
+		lblUpdate = new JLabel(textdata.getKey("lblupdatenow"));
 		lblUpdate.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -974,7 +1096,7 @@ public class LoginGUI {
 				
 				btnLogin.setEnabled(false);
 				
-				((JLabel) arg0.getComponent()).setText("In process...");
+				((JLabel) arg0.getComponent()).setText(textdata.getKey("lblupdatenowprocessing"));
 				
 				arg0.getComponent().setEnabled(false);
 				
@@ -1008,11 +1130,11 @@ public class LoginGUI {
 							updateJar=updateResult;
 							
 							//toDoOnClosing();
-							lblUpdate.setText("Done. Restart to update");
+							lblUpdate.setText(textdata.getKey("lblupdatenowrestart"));
 							
 						} else{
 
-							lblUpdate.setText("Error updating");
+							lblUpdate.setText(textdata.getKey("lblupdatenowerror"));
 							
 						}
 						
@@ -1032,15 +1154,15 @@ public class LoginGUI {
 		lblConfigurationFolder = new JLabel("Configuration folder:");
 		lblConfigurationFolder.setForeground(Color.GRAY);
 		lblConfigurationFolder.setHorizontalAlignment(SwingConstants.CENTER);
-		panelWithEverything.add(lblConfigurationFolder, "2, 8");
+		panelWithEverything.add(lblConfigurationFolder, "2, 9");
 		
 		JLabel lblAbout = new JLabel(textdata.getKey("appbriefdescription"));
-		panelWithEverything.add(lblAbout, "2, 10");
+		panelWithEverything.add(lblAbout, "2, 11");
 		lblAbout.setForeground(Color.GRAY);
 		lblAbout.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JLabel lblAcercaDe = new JLabel(textdata.getKey("btnabout") + "     ");
-		panelWithEverything.add(lblAcercaDe, "3, 10");
+		panelWithEverything.add(lblAcercaDe, "3, 11");
 		lblAcercaDe.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		lblAcercaDe.setForeground(new Color(0,110,198,255));
 		lblAcercaDe.setHorizontalAlignment(SwingConstants.RIGHT);
